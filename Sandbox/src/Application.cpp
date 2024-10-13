142f19d80a2a0a2acd420ec5e015da468e2917fb
Ilya Rybakov <47.2region@gmail.com>
2024-10-13 20:16:37 +0600

    setup basic project solution

---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,12 @@
+
+namespace Engine
+{
+
+	__declspec(dllexport) void print();
+}
+
+
+void main()
+{
+	Engine::print();
+}
---
+++ game-engine/src/test.cpp
@@ -0,0 +1,7 @@
+#include "test.h"
+#include <stdio.h>
+
+void Engine::print()
+{
+	printf("Welcome to my engine! \n");
+}
